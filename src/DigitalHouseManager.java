f3d8f8040cbaee2ca12409baf8654cda28e1040b
Alvaro <alvarogimenez_285@hotmail.com>
2018-09-01 15:51:40 -0300

    Eliminada la clase Sound
    se agrego por error

--- src/DigitalHouseManager.java
+++ src/DigitalHouseManager.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DigitalHouseManager {
+    //ATRIBUTOS
+    private List<Alumno> listaAlumnos = new ArrayList<>();
+    private List<Profesor> listaProfesores = new ArrayList<>();
+    private List<Inscripcion> listaIncripciones = new ArrayList<>();
+    private List<Curso> cursos = new ArrayList<>();
+
+    //METODOS
+    public void altaCurso(String nombre, Integer codigoCurso,Integer cupoMaximoAlumnos){
+
+
+    }
 }
